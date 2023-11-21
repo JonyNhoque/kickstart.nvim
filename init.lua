786d39d0cb90ae7384d27c6cb480246454a65d9f
joao.nhoque <joao.nhoque@metasix.com.br>
2023-11-21 18:33:49 -0300

    add neotree

--- init.lua
+++ init.lua
@@ -153,15 +153,27 @@ require('lazy').setup({
     },
   },
 
---[[  {
+  -- Neotree
+  {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      }
+  },
+
+  {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'desert'
     end,
   },
---]]
+
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
